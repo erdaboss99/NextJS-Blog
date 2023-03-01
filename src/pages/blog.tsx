7214f4b714e34fe6b04d926fee1acd0309883f26
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-01 22:13:21 +0100

    Extract each post into a separate card component

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,17 +1,23 @@
+import PostCard from 'component/PostCard';
 import { NextPage } from 'next';
 
 interface BlogPageProps {}
 
 const BlogPage: NextPage<BlogPageProps> = () => {
 	return (
-		<div className='max-w-3xl mx-auto p-5'>
-			<div className='bg-green-100 p-2 rounded'>
-				<h1 className='text-gray-900 text-3xl font-semibold'>Lorem ipsum dolor sit amet.</h1>
-				<p className='text-gray-500'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam repellendus totam
-					ducimus fuga corrupti quia expedita modi sequi eaque at.
-				</p>
-			</div>
+		<div className='max-w-3xl mx-auto p-5 space-y-5'>
+			<PostCard
+				title='This is my title for the first post on this blog'
+				description='This is the description for the first post on this blog'
+			/>
+			<PostCard
+				title='This is my title for the second post on this blog'
+				description='This is the description for the second post on this blog'
+			/>
+			<PostCard
+				title='This is my title for the third post on this blog'
+				description='This is the description for the third post on this blog'
+			/>
 		</div>
 	);
 };
