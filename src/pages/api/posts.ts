279f002902a06381c5e908f8fb82c7c3f30581de
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-03 22:52:39 +0100

    Create Posts API Endpoint

---
+++ src/pages/api/posts.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+import { NextApiHandler } from 'next';
+
+const handler: NextApiHandler = (req, res) => {
+	const { method } = req;
+
+	switch (method) {
+		case 'GET': {
+			const data = readPostsInfo();
+			return res.json({ postInfo: data });
+		}
+		default:
+			return res.status(404).send('Not Found!');
+	}
+};
+
+const readPostsInfo = () => {
+	const dirPathToRead = path.join(process.cwd(), 'posts');
+	const dirs = fs.readdirSync(dirPathToRead);
+	const data = dirs.map((filename) => {
+		const filePathToRead = path.join(process.cwd(), 'posts/' + filename);
+		const fileContent = fs.readFileSync(filePathToRead, 'utf-8');
+		return matter(fileContent).data;
+	});
+
+	return data;
+};
+
+export default handler;
