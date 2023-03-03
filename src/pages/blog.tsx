f55925f46a26dc411fd6c3b2fe65d546108836ab
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-03 22:53:23 +0100

    List data provided by post endpoint

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,23 +1,25 @@
-import PostCard from 'component/PostCard';
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import PostCard from 'component/PostCard';
 
 interface BlogPageProps {}
 
 const BlogPage: NextPage<BlogPageProps> = () => {
+	const [posts, setPosts] = useState<{ title: string; slug: string; meta: string }[]>([]);
+
+	const fetchPost = async () => {
+		const res = await fetch('/api/posts').then((data) => data.json());
+		setPosts(res.postInfo);
+	};
+	useEffect(() => {
+		fetchPost();
+	}, []);
+
 	return (
 		<div className='max-w-3xl mx-auto p-5 space-y-5'>
-			<PostCard
-				title='This is my title for the first post on this blog'
-				description='This is the description for the first post on this blog'
-			/>
-			<PostCard
-				title='This is my title for the second post on this blog'
-				description='This is the description for the second post on this blog'
-			/>
-			<PostCard
-				title='This is my title for the third post on this blog'
-				description='This is the description for the third post on this blog'
-			/>
+			{posts.map((post) => (
+				<PostCard key={post.slug} title={post.title} description={post.meta} />
+			))}
 		</div>
 	);
 };
