d5a4bd6aa414a9738454d82813fefbdbb9d09eb4
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-04 13:57:20 +0100

    Implement getStaticProps with types

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,20 +1,27 @@
-import { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { InferGetStaticPropsType, NextPage } from 'next';
 import PostCard from 'component/PostCard';
 
-interface BlogPageProps {}
+interface PostApiResponse {
+	postInfo: {
+		title: string;
+		slug: string;
+		meta: string;
+	}[];
+}
 
-const BlogPage: NextPage<BlogPageProps> = () => {
-	const [posts, setPosts] = useState<{ title: string; slug: string; meta: string }[]>([]);
+export const getStaticProps = async () => {
+	const { postInfo }: PostApiResponse = await fetch('http://localhost:3000/api/posts').then(
+		(data) => data.json(),
+	);
 
-	const fetchPost = async () => {
-		const res = await fetch('/api/posts').then((data) => data.json());
-		setPosts(res.postInfo);
+	return {
+		props: { posts: postInfo },
 	};
-	useEffect(() => {
-		fetchPost();
-	}, []);
+};
 
+type BlogPageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const BlogPage: NextPage<BlogPageProps> = ({ posts }) => {
 	return (
 		<div className='max-w-3xl mx-auto p-5 space-y-5'>
 			{posts.map((post) => (
