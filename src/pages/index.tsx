6553c58ca122f8573f2b65a359785b9cbd6c8a41
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-03-07 21:00:19 +0100

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
 	return (
 		<div>
-			<h1 className='text-3xl font-bold'>Test heading!</h1>
+			<h1 className='text-3xl font-bold text-center'>Welcome to the home page!</h1>
+			<Link className='block' href='/blog'>
+				<div className='bg-blue-100 w-[15%] mx-auto my-5 p-2 rounded text-center'>
+					<h2 className='text-gray-900 text-3xl font-semibold'>List of articles</h2>
+				</div>
+			</Link>
 		</div>
 	);
 }
